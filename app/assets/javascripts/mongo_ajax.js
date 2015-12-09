ff0e8860f8c12e4575d9ad757847c7a436465ec8
saran23 <saranya.ravichandran@mavs.uta.edu>
2015-12-09 03:12:13 +0000

    final

--- app/assets/javascripts/mongo_ajax.js
+++ app/assets/javascripts/mongo_ajax.js
@@ -1,40 +1,43 @@
 
 $( document ).ready(function() {
     var id=1;
+    var count=0;
   
     $("#btn1").one("click",function(){ 
       
       $("#btn1").hide(); //  hides the button after it has been clicked
-      $("p").replaceWith("Data is loaded at 0.5 seconds interval and displays only 20 records at a time");
-      var ajaxreq = setInterval(function() 
-      {        // on button click Ajax call is made
+      
+      $("#title").css('visibility','visible').css('opacity','1');
+      $("p").html("Data is loaded at 0.5 seconds interval and displays only 20 records at a time");
+      var ajaxreq = setInterval(function()  // Function to make multiple Ajax calls for each record 
+      {       
       $.ajax(
       { 
         type: "GET",                
         url: "/mongo/index",                     // Path to Ruby Controller action
         datatype: "json",
         async:false,
-        data: $.param({ dataset: id}),        
+        data: $.param({ dataset: id}),     //The value of Id is passed as parameter to fetch that particular record   
         success: function (data) 
         { 
           // On success of the Ajax - call Data is appended on erb file
-          if(id==1)
+          
+          $("#personTable").append(data)
+          count=count+1
+          // To display only 20 records at a time
+          if(count>20)
           {
-            $("#table").append("<table><tr><th>Seq_No</th><th>Emp_ID</th><th>Emp_Name</th><th>Dept_Name</th><th>Job_Code</th><th>Job_Title</th><th>Grade</th><th>Annual_Salary</th></tr>");
-          }
-          $("#table").append(data)
-          if(id%20==0)
-          {
-            $("#table").empty()
-            $("#table").append("<table><tr><th>Seq_No</th><th>Emp_ID</th><th>Emp_Name</th><th>Dept_Name</th><th>Job_Code</th><th>Job_Title</th><th>Grade</th><th>Annual_Salary</th></tr>");
+            console.log("count is"+count)
+            var rem_id=id-20
+            console.log("rem_id is"+rem_id)
+            $("#"+rem_id).remove()
           }
         },
       });
-      //$("table").animate({height :'0',opacity:'0',top: 1080
-   // }//, 'slow'); // Adds Animation to the displaying data records ,opacity : '0'}, 3500
-    //       //$("table").hide();
-    id=id+1
-     if(id==101)         // Check if 125 calls are completed
+      // Adds Animation to the displaying data records. In CSS opacity is set to be a lower value and we modify it in this animation.
+      $("#"+id).animate({opacity:'1'},350); 
+    id=id+1;
+     if(id==101)         // Check if 100 calls are completed
       {
         clearInterval(ajaxreq);     // stop reptitive ajax calls
       }
--- app/controllers/mongo_controller.rb
+++ app/controllers/mongo_controller.rb
@@ -4,32 +4,32 @@ require 'open-uri'
 
 class MongoController < ApplicationController
   def proj3 
-    url = 'https://datacatalog.cookcountyil.gov/api/views/hdna-35se/rows.json?accessType=DOWNLOAD'
-    result = open(url).read
-    data = JSON.parse(result)
-    puts "check1"
-    i=0
-    id=1
-    data.each do |row|
-        row.each do |lines|
-            if lines == "data"
-                row[1].each do |values|
-                    if i<100
-                        i+=1
-                        values[12]=values[12].split("\'").join("''")
-                        Employee.create!(_id: "#{id}" , emp_id: "#{values[11]}", emp_name: "#{values[12]}", dept_name: "#{values[10]}",  job_code: "#{values[13]}", job_title: "#{values[14]}",  grade: "#{values[15]}", annual_salary: "#{values[20]}")
-                        id=id+1
-                    else 
-                        break
-                    end
-                end
+    # url = 'https://datacatalog.cookcountyil.gov/api/views/hdna-35se/rows.json?accessType=DOWNLOAD'
+    # result = open(url).read
+    # data = JSON.parse(result)
+    # puts "check1"
+    # i=0
+    # id=1
+    # data.each do |row|
+    #     row.each do |lines|
+    #         if lines == "data"
+    #             row[1].each do |values|
+    #                 if i<100
+    #                     i+=1
+    #                     values[12]=values[12].split("\'").join("''")
+    #                     Employee.create!(_id: "#{id}" , emp_id: "#{values[11]}", emp_name: "#{values[12]}", dept_name: "#{values[10]}",  job_code: "#{values[13]}", job_title: "#{values[14]}",  grade: "#{values[15]}", annual_salary: "#{values[20]}")
+    #                     id=id+1
+    #                 else 
+    #                     break
+    #                 end
+    #             end
                 
-            end
-        end 
-    end
+    #         end
+    #     end 
+    # end
 
   end
-  #This Method will fetch the JSON data from the .json file ,following the path name and parse it.
+  
   def index 
   @id = params[:dataset]
   @result = Employee.where(_id: "#{@id}")
